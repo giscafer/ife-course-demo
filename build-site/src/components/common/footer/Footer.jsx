108820e60bb0aae11f436c1623b3a8dae13e2636
giscafer <giscafer@outlook.com>
2017-03-02 22:48:18 +0800

    update details

--- build-site/src/components/common/footer/Footer.jsx
+++ build-site/src/components/common/footer/Footer.jsx
@@ -4,13 +4,12 @@ export default () => {
     return (
         <footer className="footer">
             <ul className="list-inline text-center">
-                <li><a href="https://github.com/giscafer/" target="_blank">©giscafer</a></li>
-                <li className="phone">Ponitor = [ P ] rice + M [ onitor ]&nbsp;价格监测</li>
-                <li className="phone"><a href="https://github.com/giscafer/Ponitor/issues/" target="_blank">意见反馈</a></li>
-                <li className="phone"><a href="https://github.com/giscafer/Ponitor/wiki/" target="_blank">使用教程</a></li>
-                <li>用户数[58]</li>
-                <li>商品数[51]</li>
+                <li><a href="https://github.com/giscafer/" target="_blank">giscafer © 2017</a></li>
+                <li className="phone">ife-course-demo</li>
+                <li className="phone"><a href="https://github.com/giscafer/ife-course-demo/issues/" target="_blank">意见反馈</a></li>
+                <li className="phone"><a href="https://github.com/giscafer/ife-course-demo/" target="_blank">Star Me</a></li>
             </ul>
+           <p>推荐关注微信公众号：giscafer</p>
         </footer>
     )
 }
